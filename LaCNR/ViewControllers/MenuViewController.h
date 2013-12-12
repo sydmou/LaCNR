e337788a52c858cfa81a283430588259d9467a53
Marc Audefroy <audefroy.marc@gmail.com>
2013-12-12 20:21:50 +0100

    add mmdrawer

---
+++ LaCNR/ViewControllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  LaCNR
+//
+//  Created by Marc Audefroy on 12/12/13.
+//  Copyright (c) 2013 supinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
